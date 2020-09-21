02fd6c9dea512fe899e741a6b7d0dfa33ba6a419
Maxim Sidorenko <sidormax@mail.ru>
2020-09-21 23:25:07 +0300

    Аргументы программы track_list.

--- track_list.py
+++ track_list.py
@@ -1,28 +1,52 @@
 # Получает треклист по заданным параметрам.
 # track_list.py tracks.bin -s deejayde -g house -f xx.xx.2020 -t xx.xx.2020
 	# выходной файл для записи
-	# s - источник
+	# s - источник [deejayde, juno, hardwax]
 	# g - жанр
 	# f - начальная дата поиска, ранняя
 	# t - конечная дата поиска, поздняя
 
+import argparse
 import requests
+import datetime
+import sys
 import os
 import funcs
 from bs4 import BeautifulSoup
-# establishing session
+
+# Разбор командной строки.
+parser = argparse.ArgumentParser()
+parser.add_argument ('bin_result', nargs=1)
+parser.add_argument ('-s', '--source', choices=['deejayde', 'juno', 'hardwax'], default='deejayde')
+parser.add_argument ('-g', '--genre', choices=['techno', 'house', 'exclusive', 'ambient'], default='techno')
+parser.add_argument ('-f', '--from_date')
+parser.add_argument ('-t', '--to_date')
+
+namespace = parser.parse_args (sys.argv[1:])
+
+tracks_path = namespace.bin_result[0]
+source = namespace.source
+genre = namespace.genre
+from_date = namespace.from_date
+to_date = namespace.to_date
+
+# print(tracks_path)
+# print(source)
+# print(genre)
+# print(from_date)
+# print(to_date)
+
+# todo Проверки соответствия источника и жанра.
+# sys.exit()
+
 s = requests.Session() 
 s.headers.update({
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'
     })
 
-# Определяем диапазон страниц которые соответствуют запросу по дате.
+# todo Определяем диапазон страниц которые соответствуют запросу по дате.
 # from_page = funcs.get_number_page_by_date(date)
 # to_page = funcs.get_number_page_by_date(date)
-
-# todo args
-tracks_path = 'tracks.bin'
-source = "deejayde"
 
 album_links_path = 'album_links.bin'
 from_page = 1
